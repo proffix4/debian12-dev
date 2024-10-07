e81468b2e9f1572e675ea8d9ff3af7ac9ce12897
proffix4 <proffix4@gmail.com>
2024-10-07 23:15:52 +0500

    update

---
+++ other/mate/mate_install.sh
@@ -0,0 +1,4 @@
+#sudo apt -y install task-mate-desktop
+#sudo apt install mate-desktop-environment -y
+sudo apt install mate-desktop-environment-extras -y
+sudo apt install lightdm lightdm-gtk-greeter -y
---
+++ other/xfce/xfce_install.sh
@@ -0,0 +1,4 @@
+sudo apt update && sudo apt upgrade -y
+#sudo apt install task-xfce-desktop -y
+sudo apt install xfce4 xfce4-goodies -y
+sudo apt install lightdm -y
